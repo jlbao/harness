e3c670e489064e1943cecae522b81b6223e8094f
Gary Pollice <gpollice@cs.wpi.edu>
2012-01-21 15:14:27 -0500

    Added more to the TestDirectory class. New tests too.

--- lib/test_directory.rb
+++ lib/test_directory.rb
@@ -1,6 +1,6 @@
 ##
-# This class encapsulates things we can do on the test harness's "test"
-# directory.
+# This class encapsulates things we can do on a directory in the test
+# harness.
 #
 # Author: gpollice@cs.wpi.edu
 # Version: 20-Jan-2012
@@ -8,10 +8,11 @@
 
 class TestDirectory
   #
-  # Constructor takes the harness directory path. By default, it
+  # Constructor takes the directory path. By default, it is the
+  # current path.
   #
-  def initialize harness_path = '.'
-    @test_path = "#{harness_path}/test"
+  def initialize directory_path = '.'
+    @dir_path = "#{directory_path}"
   end
   
   #
@@ -19,21 +20,34 @@ class TestDirectory
   #
   def files
     entries = []
-    Dir.entries(@test_path).each do |e|
-      puts e
-      entries.push e if File.file? "#{@test_path}/#{e}"
+    Dir.entries(@dir_path).each do |e|
+      entries.push e if File.file? "#{@dir_path}/#{e}"
     end
     entries
   end
   
   #
-  # return the suubdirectories
+  # Return the suubdirectories of this directory.
   #
   def subdirectories
     entries = []
-    Dir.entries(@test_path).each do |e|
-      entries.push e if File.directory? "#{@test_path}/#{e}"
+    Dir.entries(@dir_path).each do |e|
+      entries.push e if File.directory? "#{@dir_path}/#{e}"
     end
     entries - [".", ".."]
+  end
+  
+  #
+  # Move a file from one (sub)directory to another, possibly renaming
+  # the file in the process.
+  #
+  def move(file_name, from_sub, to_sub, new_name=nil)
+    from_path = "#{@dir_path}/#{from_sub}/#{file_name}"
+    new_name = file_name if new_name.nil?
+    permissions = File.stat(from_path).mode & 0o777
+    to_path = "#{@dir_path}/#{to_sub}/#{new_name}"
+    IO.copy_stream(from_path, to_path)
+    File.chmod(permissions, to_path)
+    File.delete from_path
   end
 end
--- unittest/test/test_directory_test.rb
+++ unittest/test/test_directory_test.rb
@@ -9,11 +9,10 @@ require_relative '../../lib/test_directory.rb'
 
 class TestDirectoryTest < Test::Unit::TestCase
   def setup
-    @td = TestDirectory.new "../harness"
+    @td = TestDirectory.new "../harness/test"
   end
   
-  def teardown
-    
+  def teardown 
     hd = "../harness"
     `rm -rf #{hd}/*`
     dirs = ["results", "results/failed", "results/passed", "results/unclassified",
@@ -46,5 +45,41 @@ class TestDirectoryTest < Test::Unit::TestCase
     assert(sd.find "new")
     assert(sd.find "passed")
     assert(sd.find "unclassified")
+  end
+  
+  #
+  # 4. Verify that the correct set of files are returned for
+  #    the directory.
+  def test_files
+    `touch ../harness/test/foo ../harness/test/bar`
+    f = @td.files
+    assert_equal(2, f.length)
+    assert(f.find "foo")
+    assert(f.find "bar")
+  end
+  
+  #
+  # 5. Test moving a file from one subdirectory (path) to
+  #    another subdirectory (path).
+  def test_move_file_in_subdirectory
+    `touch ../harness/test/new/foo`
+    nd = TestDirectory.new "../harness/test/new"
+    assert(nd.files.find "foo")
+    @td.move(:foo, :new, :unclassified)
+    assert(File.exist?("../harness/test/unclassified/foo"))
+    assert(!File.exist?("../harness/test/new/foo"))
+  end
+  
+  #
+  # 6. Test to ensure that the permissions on a moved file
+  #    are the same as they were before the move.
+  #
+  def test_moved_file_permissions
+    `touch ../harness/test/new/foo;chmod 754 ../harness/test/new/foo`
+    permissions = File.stat("../harness/test/new/foo").mode
+    nd = TestDirectory.new "../harness/test/new"
+    @td.move(:foo, :new, :unclassified)
+    p1 = File.stat("../harness/test/unclassified/foo").mode
+    assert_equal(permissions, p1)
   end
 end
